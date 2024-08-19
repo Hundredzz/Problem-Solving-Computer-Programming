70cb1192e3f5e74115d5451e1f35019e57a37f42
Hundredz <artasitara@gmail.com>
2024-08-19 22:38:57 +0700

    bad code stair

--- stair.py
+++ stair.py
@@ -5,22 +5,36 @@ def main():
     stair_num = int(input())
     all_height = 0
     old_height = 0
-    count = 0
     height = 0
     kao = 0
+    check = True
     for _ in range(stair_num):
-        if all_height > height_can:
-            kao += count - 1
-            all_height -= height_can
-            count = 1
         height = int(input())
+        if all_height > height_can:
+            kao += 1
+            all_height -= old_height
+            if height + all_height > height_can:
+                kao += 1
+                all_height = 0
+        elif all_height == height_can and height_can:
+            kao += 1
+            all_height = 0
         if height > height_can:
             kao = 0
+            check = False
             break
-        elif height == height_can:
+        if height == height_can:
             kao += 1
         else:
-            all_height += height_can
-            count += 1
             old_height = all_height
+            all_height += height
+    if check:
+        if all_height > height_can:
+            kao += 2
+        elif 0 < all_height <= height_can:
+            kao += 1
+    if kao:
+        print(kao)
+    else:
+        print("NO")
 main()
