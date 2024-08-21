3941613021af5d151a4687d6b86d504bee375ac2
Hundredz <artasitara@gmail.com>
2024-08-21 16:22:45 +0700

    bad pass lotto

--- lotto.py
+++ lotto.py
@@ -1,6 +1,8 @@
 """lottery"""
 def near(first):
     """near"""
+    near_c1 = ""
+    near_c2 = ""
     if first == "999999":
         near_f = "000000" + " " + "999998"
     elif first == "000000":
@@ -10,26 +12,36 @@ def near(first):
             near_f = first[:first.rfind("0")+1] + str(int(first[first.rfind("0")+1:]) - 1) + " "\
 + first[:first.rfind("0")+1] + str(int(first[first.rfind("0")+1:])+1)
         else:
-            near_f = str(int(first) - 1) + " " +str(int(first)+1)
+            near_c1 = str(int(first) - 1)
+            near_c1 = "0"*(6-len(near_c1)) + near_c1
+            near_c2 = str(int(first)+1)
+            near_c2 = "0"*(6-len(near_c2)) + near_c2
+            near_f = near_c1 + " " + near_c2
     return near_f
 def main():
     """function"""
     first = input()
     near_f = near(first)
     two_l = input()
-    three_f = input() + " " + input()
-    three_l = input() + " " + input()
+    three_f1 = input()
+    three_f2 = input()
+    three_l1 = input()
+    three_l2 = input()
     lotto = input()
     prize = 0
     if lotto == first:
         prize += 6000000
-    if lotto[-2:] in two_l:
-        prize += 2000
-    if lotto[:3] in three_f:
-        prize += 4000
-    if lotto[-3:] in three_l:
-        prize += 4000
     if lotto in near_f:
         prize += 100000
+    if lotto[-2:] == two_l:
+        prize += 2000
+    if lotto[:3] == three_f1:
+        prize += 4000
+    if lotto[:3] == three_f2:
+        prize += 4000
+    if lotto[-3:] == three_l1:
+        prize += 4000
+    if lotto[-3:] == three_l2:
+        prize += 4000
     print(prize)
 main()
