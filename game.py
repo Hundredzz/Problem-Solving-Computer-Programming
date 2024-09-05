a010f8f9fecc23a1867d1c51ee7ccf6f550ca574
Hundredz <artasitara@gmail.com>
2024-09-05 17:52:29 +0700

    game

---
+++ game.py
@@ -0,0 +1,11 @@
+"""code"""
+def main():
+    """function"""
+    a_win = int(input())
+    b_win = int(input())
+    if a_win%3 == b_win%3:
+        print(a_win%3)
+    else:
+        print("Error")
+main()
+ 
